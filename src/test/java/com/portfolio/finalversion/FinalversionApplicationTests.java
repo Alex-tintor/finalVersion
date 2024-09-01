6d712c240db8768ed67457204cfeaa869f62419d
Alex-tintor:p <jalex_moral@hotmail.com>
2024-08-31 20:19:23 -0500

    Initial commit

---
+++ src/test/java/com/portfolio/finalversion/FinalversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.finalversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
